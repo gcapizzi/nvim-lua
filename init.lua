28ffe967c5195e77f500866b32026df6f98205e4
Giuseppe Capizzi <g.capizzi@gmail.com>
2022-02-17 15:10:00 +0100

    Add lsp_dynamic_workspace_symbols Telescope mapping

--- init.lua
+++ init.lua
@@ -27,6 +27,7 @@ return require('packer').startup(function()
 			vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true })
 			vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true })
 			vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true })
+			vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>', { noremap = true })
 		end
 	}
 	use {
