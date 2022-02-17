a7a053c62b34a187f5dff6815d5396498d14f1a0
Giuseppe Capizzi <g.capizzi@gmail.com>
2022-02-17 15:09:44 +0100

    Remove help_tags Telescope mapping

--- init.lua
+++ init.lua
@@ -27,7 +27,6 @@ return require('packer').startup(function()
 			vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true })
 			vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true })
 			vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true })
-			vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true })
 		end
 	}
 	use {
