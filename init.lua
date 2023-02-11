9a8c945956cba2fcf4c7b1dffff9fbf931043d89
Giuseppe Capizzi <g.capizzi@gmail.com>
2023-02-11 19:42:09 +0100

    Use lualine's tabline

--- init.lua
+++ init.lua
@@ -95,7 +95,17 @@ return require('lazy').setup({
       require('lualine').setup {
         options = {
           theme = 'dracula',
-          icons_enabled = false
+          icons_enabled = false,
+          section_separators = '',
+          component_separators = ''
+        },
+        tabline = {
+          lualine_a = {'buffers'},
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {'tabs'}
         }
       }
     end
