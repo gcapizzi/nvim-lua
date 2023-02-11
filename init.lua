26b5ec0a79e6d5840ac022e2930e6b392e4ce847
Giuseppe Capizzi <g.capizzi@gmail.com>
2023-02-11 18:40:58 +0100

    Add vim and lua Treesitter parsers

--- init.lua
+++ init.lua
@@ -121,7 +121,7 @@ return require('lazy').setup({
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup {
-        ensure_installed = {"go", "rust"},
+        ensure_installed = {"vim", "lua", "go", "rust"},
         highlight = {
           enable = true
         },
