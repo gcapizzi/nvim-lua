2fcd09c845979ac8f78270a55d86d7424809951c
Giuseppe Capizzi <g.capizzi@gmail.com>
2024-07-12 14:37:46 +0100

    Add substitute.nvim

--- init.lua
+++ init.lua
@@ -253,6 +253,19 @@ return require('lazy').setup({
       vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
     end
   },
+  {
+    "gbprod/substitute.nvim",
+    config = function()
+      require("substitute").setup({
+        on_substitute = require("yanky.integration").substitute(),
+      })
+
+      vim.keymap.set("n", "s", require('substitute').operator, { noremap = true })
+      vim.keymap.set("n", "ss", require('substitute').line, { noremap = true })
+      vim.keymap.set("n", "S", require('substitute').eol, { noremap = true })
+      vim.keymap.set("x", "s", require('substitute').visual, { noremap = true })
+    end
+  },
   'vim-test/vim-test',
   'wellle/targets.vim',
   'justinmk/vim-dirvish',
