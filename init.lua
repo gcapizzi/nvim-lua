406a8fc51e76ec6d180c09d4b564a44513b81400
Giuseppe Capizzi <g.capizzi@gmail.com>
2024-07-26 18:18:22 +0100

    Try nvim-cmp

--- init.lua
+++ init.lua
@@ -160,9 +160,31 @@ return require("lazy").setup({
     end
   },
   {
-    "vim-scripts/VimCompletesMe",
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
     config = function()
-      vim.o.completeopt = "menu"
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        })
+      })
     end
   },
   {
