94669bded6b16ec3e4d4950957412433d66f08cf
Giuseppe Capizzi <g.capizzi@gmail.com>
2024-08-25 14:04:36 +0100

    Update lualine.nvim config

--- init.lua
+++ init.lua
@@ -147,7 +147,7 @@ return require("lazy").setup({
     end
   },
   {
-    "hoob3rt/lualine.nvim",
+    "nvim-lualine/lualine.nvim",
     config = function()
       require("lualine").setup {
         options = {
@@ -158,10 +158,6 @@ return require("lazy").setup({
         },
         tabline = {
           lualine_a = {"buffers"},
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
           lualine_z = {"tabs"}
         }
       }
