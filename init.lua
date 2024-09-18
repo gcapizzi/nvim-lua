6fda597566663e215f75f64d5a8930fe4800eb33
Giuseppe Capizzi <g.capizzi@gmail.com>
2024-09-18 13:01:55 +0200

    Only confirm explicitly selected completions with <CR>

--- init.lua
+++ init.lua
@@ -199,7 +199,7 @@ return require("lazy").setup({
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
