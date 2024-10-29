229551138e20f4eefe840c37619816e5045d2a70
Giuseppe Capizzi <g.capizzi@gmail.com>
2024-10-29 11:17:25 +0000

    Show file path in lualine

--- init.lua
+++ init.lua
@@ -183,6 +183,9 @@ return require("lazy").setup({
           icons_enabled = false,
           section_separators = "",
           component_separators = ""
+        },
+        sections = {
+          lualine_c = {{ 'filename', path = 1 }}
         }
       }
     end
