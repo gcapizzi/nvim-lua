a852ead603f15f0b292ce4f1fab164762c680c0d
Giuseppe Capizzi <g.capizzi@gmail.com>
2026-04-05 22:37:45 +0200

    Formatting

--- init.lua
+++ init.lua
@@ -137,7 +137,7 @@ require("oil").setup({
   delete_to_trash = true
 })
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
 
 -- vim-test
 
