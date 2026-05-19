c224b4eafac85cc5f0e44ef9cb4980009f9651fa
Giuseppe Capizzi <g.capizzi@gmail.com>
2026-05-19 23:56:27 +0100

    vim.lsp.buf.format already has a default 1s timeout

--- init.lua
+++ init.lua
@@ -50,7 +50,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
       vim.api.nvim_create_autocmd("BufWritePre", {
         buffer = ev.buf,
         callback = function()
-          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id })
         end,
       })
     end
